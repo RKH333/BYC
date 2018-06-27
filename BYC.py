a3832ec22b90b136c17c6348b2e3c3bc32ecb9f3
Roberto <robchenz0633@gmail.com>
2018-06-27 00:12:32 -0600

    Funcionalidad del nivel 1
    Se agregan las siguientes funciones.
    +Repetidos
    + IngresarDatos
    +DeployMessage
    +DeployWinnerMessage
    
    Se debe de probar las funciones por aparte, no llamar la función BYC para realizar las pruebas

--- BYC.py
+++ BYC.py
@@ -10,23 +10,39 @@ def Reglas():
     print("Vaca: Si una cifra está presente pero se encuentra en un lugar equivocado")
     print("Al adivinarse el número se termina la partida")
     print("Los números a utilizar son del 0-9\n")
+    print("El número a adivinar no puede empezar con el número 0")
     return
 
+#Verifica que los numeros generados no estén repetidos
+#Recibe una lista
+#Devuelve un True si hay repetidos, y False si no hay repetidos
+def Repetidos(lista):
+    #Se verifica con el largo de la lista
+    return(len(lista) != len(set(lista))) 
+
+    
 # Generador de numeros aleatorios para cierta dificultad
     # Falta verificar que no se repitan numeros
-    # Son retornados como una lista de numeros separados
+    # Son retornados como una lista de numeros separados ejm: [3,2,4,1]
 def Generador(num):
+    repetido= True
     if (num==1):
-        rando= randint(0,999)
-        lista= [int(i) for i in rando]
+        while(repetido == True):
+            rando= randint(100,999)
+            lista= [int(i) for i in str(rando)]
+            repetido= Repetidos(lista)
         return (lista)
     elif (num==2):
-        randint(0,9999)
-        lista= [int(i) for i in rando]
+        while(repetido == True):
+            rando= randint(1000,9999)
+            lista= [int(i) for i in str(rando)]
+            repetido= Repetidos(lista)
         return (lista)
     elif (num==3):
-        rando = randint(0,999)
-        lista= [int(i) for i in rando]
+        while(repetido == True):
+            rando= randint(10000,99999)
+            lista= [int(i) for i in str(rando)]
+            repetido= Repetidos(lista)
         return (lista)
     else:
         return ([])
@@ -49,13 +65,31 @@ def Evaluacion(numero,var,respuesta):
 def Nivel_1():
     print("Juego Fácil 3 dígitos")
     contador=0
-    var=False
+    var= False
     respuesta= Generador(1)
-    while (var!=False):
-        numero= input("Ingrese el numero: ")
-        lista= [int(i) for i in numero]
-        # El resultado es una tupla, el primer valor son toros, el segundo son vacas
+    print(respuesta)
+    while (var==False):
+        numero= IngresarDatos()
         resultado= Evaluacion(numero,var,respuesta)
+        DeployMessage(resultado)
+        if(resultado[0]==3):
+            var= True
+    else:
+        DeployWinnerMessage(respuesta)
+
+
+
+def IngresarDatos():
+    numero= input("Ingrese el numero: ")
+    lista= [int(i) for i in str(numero)]
+    return (lista)
+    
+    
+def DeployMessage(resultado):
+    print("Usted tiene "+ str(resultado[0])+ " toros y " + str(resultado[1]) +" vacas")
+
+def DeployWinnerMessage(respuesta):
+    print("Usted ha ganado la respuesta es: " + str(respuesta))
     
 
 def BYC():
@@ -64,14 +98,14 @@ def BYC():
     opcion= int(input("Seleccione una opción: "))
     if (opcion == 1):
         nivel= input("Ingrese la dificultad: ")
-##        if (nivel == 1):
-##            Nivel_1()
-##        elif (nivel == 2):
-##            Nivel_2
-##        elif (nivel ==3):
-##            Nivel_3
-##        else:
-##            print ("Elija de nuevo la dificultad: ")
+        if (nivel == 1):
+            Nivel_1()
+        elif (nivel == 2):
+            Nivel_2()
+        elif (nivel ==3):
+            Nivel_3()
+        else:
+            print ("Elija de nuevo la dificultad: ")
     elif (opcion == 2):
         Reglas()
         BYC()
